c727aa56666f6a4a6a87c4b52fc29f4a8d2b1a9c
ELENI JAMILLE GOMES SARAIVA <ellennygomes@gmail.com>
2024-03-29 15:23:11 -0300

    incluindo o javascript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("botao_enviar").addEventListener("click", validaFormulario)
+
+function validaFormulario() {
+  if (document.getElementById("nome").value != ""
+    &&
+    document.getElementById("email").value != "") {
+    alert("Prontinho! Voce recebera as novidades por email.")
+  }
+  else {
+    alert("Por favor, preencha os campos nome e mail!")
+  }
+}
